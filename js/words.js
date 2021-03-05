fd6bc0260ec52c359ab628d2b512a45b4ba7e96b
KOCH-Thomas <Koch.thomas@outlook.com>
2021-03-05 15:35:50 +0100

    first commit

---
+++ js/words.js
@@ -0,0 +1,42 @@
+// TALBEAU DES DIFFERENTS MOTS (UN MOT SERA CHOISI AU HASARD GRACE A SON INDEX ( INDEX = NUMERO ))
+const words = [
+
+    "CHAUSSURE",                // 0
+    "VERRE",                    // 1
+    "CHAISE",                   // 2
+    "ORDINATEUR",               // 3
+    "JEUX",                     // 4
+    "TELEPHONE",                // 5
+    "CHATEAU",                  // 6
+    "CASQUE",                   // 7
+    "CASQUETTE",                // 8
+    "ARMOIRE",                  // 9
+    "MUSIQUE",                  // 10    
+    "BARBECUE",                 // 11 
+    "CLAVIER",                  // 12 
+    "TELEVISION",               // 13 
+    "NETFLIX",                  // 14
+    "AMAZON",                   // 15 
+    "PORTE",                    // 16 
+    "POSTER",                   // 17 
+    "TABLEAU",                  // 18 
+    "TASSE",                    // 19 
+    "RESTAURANT",               // 20
+    "POUBELLE",                 // 21
+    "LAMPADAIRE",               // 22
+    "LAMPE",                    // 23
+    "MONOPOLY",                 // 24
+    "CHARGEUR",                 // 25
+    "SAMSUNG",                  // 26
+    "MICROSOFT",                // 27
+    "APPLE",                    // 28
+    "STYLO",                    // 29
+    "MOUCHOIR",                 // 30
+    "SACHET",                   // 31
+    "RADIATEUR",                // 32
+    "MEUBLE",                   // 33
+    "VACANCE",                  // 34
+    "MALDIVES",                 // 35
+    
+    
+    ] 
